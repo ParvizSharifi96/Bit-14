424d8b49034ce4a1b447e541bb03de3c1b7a6ec1
ParvizSharifi96 <parviz.sharifi1996@gmail.com>
2023-03-30 01:08:19 +0430

    Coding of the news section

--- app/src/main/java/com/example/bit_14/apiManager/ApiManager.kt
+++ app/src/main/java/com/example/bit_14/apiManager/ApiManager.kt
@@ -1,5 +1,10 @@
 package com.example.bit_14.apiManager
 
+import android.os.Message
+import com.example.bit_14.apiManager.model.NewsData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -22,4 +27,35 @@ class ApiManager {
         apiService = retrofit.create(ApiService::class.java)
 
     }
+
+    fun getNews(apiCallback: ApiCallback<ArrayList<Pair<String, String>>>){
+
+
+        apiService.getTopNews().enqueue(object :Callback<NewsData>{
+            override fun onResponse(call: Call<NewsData>, response: Response<NewsData>) {
+
+                val data = response.body()!!
+                val dataToSend: ArrayList<Pair<String, String>> = arrayListOf()
+                data.data.forEach {
+                    dataToSend.add(Pair(it.title, it.url))
+                }
+                apiCallback.onSuccess(dataToSend)
+            }
+            override fun onFailure(call: Call<NewsData>, t: Throwable) {
+                apiCallback.onError( t.message!! )
+
+            }
+        })
+    }
+
+
+    interface ApiCallback<T>{
+
+        fun onSuccess(data : T)
+        fun onError(errorMessage: String)
+
+    }
+
+
+
 }
--- app/src/main/java/com/example/bit_14/features/MarketActivity.kt
+++ app/src/main/java/com/example/bit_14/features/MarketActivity.kt
@@ -1,16 +1,61 @@
 package com.example.bit_14.features
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.bit_14.apiManager.ApiManager
 import com.example.bit_14.databinding.ActivityMarketBinding
 
 class MarketActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMarketBinding
+   lateinit var binding: ActivityMarketBinding
+    lateinit var dataNews: ArrayList<Pair<String, String>>
+    val apiManager = ApiManager()
     override fun onCreate(savedInstanceState: Bundle?) {
         binding= ActivityMarketBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.layoutToolbar.toolbar.title = "Bit-14"
+
+        initUi()
+
+    }
+
+    private fun initUi(){
+
+        getNewsFromApi()
+
+    }
+
+    private fun getNewsFromApi() {
+
+        apiManager.getNews(object : ApiManager.ApiCallback<ArrayList<Pair<String, String>>> {
+
+            override fun onSuccess(data: ArrayList<Pair<String, String>>) {
+                dataNews = data
+                refreshNews()
+            }
+
+            override fun onError(errorMessage: String) {
+                Toast.makeText(this@MarketActivity, "error => " + errorMessage, Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        })
+
+    }
+    private fun refreshNews() {
+
+        val randomAccess = (0..49).random()
+        binding.layoutNews.txtNews.text = dataNews[randomAccess].first
+        binding.layoutNews.imgNews.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(dataNews[randomAccess].second))
+            startActivity(intent)
+        }
+        binding.layoutNews.txtNews.setOnClickListener {
+            refreshNews()
+        }
 
     }
 }
