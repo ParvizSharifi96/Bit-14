900c2f1ac2eb742b8beb5b573b2b03731e163799
ParvizSharifi96 <parviz.sharifi1996@gmail.com>
2023-03-29 15:17:07 +0430

    Add dependencies&themes&colors

--- app/src/main/java/com/example/bit_14/CoinActivity.kt
+++ app/src/main/java/com/example/bit_14/CoinActivity.kt
@@ -2,12 +2,13 @@ package com.example.bit_14
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bit_14.databinding.ActivityCoinBinding
 import com.example.bit_14.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+class CoinActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCoinBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding= ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityCoinBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
---
+++ app/src/main/java/com/example/bit_14/MarketActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bit_14
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bit_14.databinding.ActivityMainBinding
+
+class MarketActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
