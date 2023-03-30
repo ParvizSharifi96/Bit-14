b0e6b406a1c71a575061f6cb3eb8ce7471ec23a9
ParvizSharifi96 <parviz.sharifi1996@gmail.com>
2023-03-31 01:06:12 +0430

    Get chart information

--- app/src/main/java/com/example/bit_14/apiManager/ApiManager.kt
+++ app/src/main/java/com/example/bit_14/apiManager/ApiManager.kt
@@ -1,6 +1,7 @@
 package com.example.bit_14.apiManager
 
 import android.os.Message
+import com.example.bit_14.apiManager.model.ChartData
 import com.example.bit_14.apiManager.model.CoinsData
 import com.example.bit_14.apiManager.model.NewsData
 import retrofit2.Call
@@ -10,10 +11,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 
-const val BASE_URL = "https://min-api.cryptocompare.com/data/"
-const val BASE_URL_IMAGE = "https://www.cryptocompare.com"
-const val API_KEY = "authorization: Apikey 98ac91fe0448e6e73045b9ac770b319602951c730179f0dee37ed6d3c58b767f"
-const val BASE_URL_TWITTER = "https://twitter.com/"
+
 class ApiManager {
     private val apiService: ApiService
 
@@ -48,8 +46,6 @@ class ApiManager {
             }
         })
     }
-
-
     fun getCoinsList( apiCallback: ApiCallback<List<CoinsData.Data>> ) {
 
         apiService.getTopCoins().enqueue( object :Callback<CoinsData> {
@@ -66,6 +62,82 @@ class ApiManager {
 
     }
 
+    fun getChartData(
+        symbol: String,
+        period: String,
+        apiCallback: ApiCallback<Pair<List<ChartData.Data>, ChartData.Data?>>
+    ) {
+
+        var histoPeriod = ""
+        var limit = 30
+        var aggregate = 1
+
+        when (period) {
+
+            HOUR -> {
+
+                histoPeriod = HISTO_MINUTE
+                limit = 60
+                aggregate = 12
+
+            }
+
+            HOURS24 -> {
+
+                histoPeriod = HISTO_HOUR
+                limit = 24
+            }
+
+            MONTH -> {
+                histoPeriod = HISTO_DAY
+                limit = 30
+            }
+
+            MONTH3 -> {
+                histoPeriod = HISTO_DAY
+                limit = 90
+            }
+
+            WEEK -> {
+                histoPeriod = HISTO_HOUR
+                aggregate = 6
+
+            }
+
+            YEAR -> {
+                histoPeriod = HISTO_DAY
+                aggregate = 13
+
+            }
+
+            ALL -> {
+
+                histoPeriod = HISTO_DAY
+                aggregate = 30
+                limit = 2000
+            }
+
+        }
+        apiService.getChartData(histoPeriod, symbol, limit, aggregate)
+            .enqueue(object : Callback<ChartData> {
+                override fun onResponse(call: Call<ChartData>?, response: Response<ChartData>?) {
+
+                    val dataFull = response?.body()!!
+                    val data1 = dataFull.data
+                    val data2 = dataFull.data.maxByOrNull { it.close.toFloat() }
+                    val returningData = Pair(data1, data2)
+                    apiCallback.onSuccess(returningData)
+                }
+
+                override fun onFailure(call: Call<ChartData>?, t: Throwable?) {
+                    apiCallback.onError(t?.message!!)
+                }
+
+
+            })
+
+
+    }
 
     interface ApiCallback<T>{
 
